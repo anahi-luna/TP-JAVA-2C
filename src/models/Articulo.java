ef471aed28e8b007119649d6c92120017720cacf
Luz Milena Gonzalez <luzg2480@gmail.com>
2025-11-14 01:29:36 -0300

    subo una nueva carpeta para trabajar

---
+++ src/models/Articulo.java
@@ -0,0 +1,53 @@
+package models;
+
+public class Articulo {
+	private String codigo;
+	private String nombreDescripcion;
+	private double precio;
+	private int stock;
+	
+	public Articulo(String codigo, String nombreDescripcion, double precio, int stock) {
+		super();
+		this.codigo = codigo;
+		this.nombreDescripcion = nombreDescripcion;
+		this.precio = precio;
+		this.stock = stock;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNombreDescripcion() {
+		return nombreDescripcion;
+	}
+
+	public void setNombreDescripcion(String nombreDescripcion) {
+		this.nombreDescripcion = nombreDescripcion;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	public int getStock() {
+		return stock;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+	
+	
+	
+	
+	
+}
