ed0671939c4e9e1368dfc5f9314f3133054e16fb
daniiespindola <riverplate.dani33@gmail.com>
2025-11-20 20:43:54 -0300

    modifico el contenedor de usuario

--- AplicacionBack/src/container/UsuarioContenedor.java
+++ AplicacionBack/src/container/UsuarioContenedor.java
@@ -1,49 +1,46 @@
 package container;
-import modelo.Usuario;
-import modelo.UsuarioTipo;
-import java.util.HashMap;
-import java.util.Map;
+
+import models.Usuario;
+import enums.UsuarioTipo;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UsuarioContenedor {
+    private static UsuarioContenedor instancia;
+    private List<Usuario> usuarios;
 
-	public class UsuarioContenedor {
-	    private static UsuarioContenedor instancia;
-	    private Map<String, Usuario> usuarios; 
-	    private UsuarioContenedor() {
-	        this.usuarios = new HashMap<>();
-	        // Agregar algunos datos iniciales para prueba
-	        agregarUsuario(new Usuario("admin", "1234", UsuarioTipo.EMPLEADO));
-	        agregarUsuario(new Usuario("clienteA", "passA", UsuarioTipo.CLIENTE));
-	        agregarUsuario(new Usuario("clienteB", "passB", UsuarioTipo.CLIENTE));
-	    }
+    private UsuarioContenedor() {
+        this.usuarios = new ArrayList<>();
+        agregarUsuario(new Usuario("admin", "1234", UsuarioTipo.EMPLEADO));
+        agregarUsuario(new Usuario("clienteA", "passA", UsuarioTipo.CLIENTE));
+        agregarUsuario(new Usuario("clienteB", "passB", UsuarioTipo.CLIENTE));
+    }
 
-	    public static UsuarioContenedor getInstancia() {
-	        if (instancia == null) {
-	            instancia = new UsuarioContenedor();
-	        }
-	        return instancia;
-	    }
+    public static UsuarioContenedor getInstancia() {
+        if (instancia == null) {
+            instancia = new UsuarioContenedor();
+        }
+        return instancia;
+    }
 
-	    // Métodos del Contenedor
-	   
-	    public boolean agregarUsuario(Usuario usuario) {
-	        if (usuarios.containsKey(usuario.getNombreUsuario())) {
-	            return false;
-	        }
-	        usuarios.put(usuario.getNombreUsuario(), usuario);
-	        return true;
-	    }
+    public boolean agregarUsuario(Usuario usuario) {
+        if (getUsuario(usuario.getNombreUsuario()) != null) {
+            return false;
+        }
+        usuarios.add(usuario);
+        return true;
+    }
 
-	    
-	    public Usuario getUsuario(String nombreUsuario) {
-	        return usuarios.get(nombreUsuario);
-	    }
+    public Usuario getUsuario(String nombreUsuario) {
+        for (Usuario u : usuarios) {
+            if (u.getNombreUsuario().equals(nombreUsuario)) {
+                return u;
+            }
+        }
+        return null;
+    }
 
-	   
-	    public Map<String, Usuario> listarTodos() {
-	        return usuarios;
-	    }
-
-	}
+    public List<Usuario> listarTodos() {
+        return usuarios;
+    }
 }
-
